0f04d40cd7f39888e1226eb63325e4538ceba956
joedanh <johul2cc@gmail.com>
2013-06-05 21:36:23 +0200

    changed to wikipedia code base

--- src/ShellSort.java
+++ src/ShellSort.java
@@ -2,39 +2,40 @@ import java.util.Arrays;
 
 public class ShellSort 
 {
-	//main program
     public static void main(String []args) 
     {
-        int[] a = {1,3,5,7,8,0,2,4,6}; //use random for next version.
-        System.out.println( Arrays.toString(a) );
-        System.out.println( Arrays.toString(shellSort(a)) );
+    	/**
+    	 * Make an array and prints array representaion
+    	 */
+        int[] array = {111,3,5,7,8,0,2,4,6};
+        System.out.println( Arrays.toString(array) ); 
+        System.out.println( Arrays.toString(shellSort(array)) );
     }
-
-    //Shell sort method algorithm associated with the whole class 
+    /**
+     * Static shared method
+     */
     public static int[] shellSort(int[] array)
     {
-        int h = 1;
-        while (h < array.length) h = 3*h + 1;
-        while (h > 0)
+        int gap = 1;
+        int i;
+    	/**
+    	 * Do an insertion sort for each gap size.  
+    	 */
+        for (i = gap; i < array.length; i += 1)
         {
-            h = h/3;
-            for (int k = 0; k < h; k++)
-            {
-                for (int i = h+k; i < array.length; i+=h)
-                {
-                    int key = array[i];
-                    int j = i-h;
-                    while (j>=0 && array[j] > key)
-                    {
-                        array[j+h] = array[j];
-                        j-=h;
-                    }
-                    array[j+h] = key;
-                    //-> invariant: array[0,h,2*h..j] is sorted
-                }
-            }
-            //->invariant: each h-sub-array is sorted
+        	int tmp = array[i];
+        	int j = i;
+        	for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+        	{
+        		array[j] = array[j - gap];
+        	}
+        	array[j] = tmp;        	
         }
+        
+        /**
+        * all sets h-sorted, now decrease set size
+        */
+        gap = gap / 3;
         return array;
-    };
+    }
 }
